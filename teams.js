dfe17b0d3a43c603f7fbc87cb987f4ddf75a5ebb
LuXoN1999 <llovretic99@gmail.com>
2022-05-23 21:53:52 +0200

    Created Main HTML file and filled teams JS file with data

---
+++ teams.js
@@ -0,0 +1,96 @@
+var teamsData = {
+    "teams": [
+        {
+            "name": "HNK Hajduk Split",
+            "location": "Split",
+            "stadium": "Poljud",
+            "goals": 64,
+            "goalsconceded": 31,
+            "possessionpercentage": 55.8,
+            "cleansheets": 18
+        },
+        {
+            "name": "GNK Dinamo Zagreb",
+            "location": "Zagreb",
+            "stadium": "Maksimir",
+            "goals": 68,
+            "goalsconceded": 22,
+            "possessionpercentage": 62.6,
+            "cleansheets": 20
+        },
+        {
+            "name": "NK Osijek",
+            "location": "Osijek",
+            "stadium": "Gradski Vrt",
+            "goals": 49,
+            "goalsconceded": 29,
+            "possessionpercentage": 49.2,
+            "cleansheets": 17
+        },
+        {
+            "name": "NK Rijeka",
+            "location": "Rijeka",
+            "stadium": "Rujevica",
+            "goals": 71,
+            "goalsconceded": 51,
+            "possessionpercentage": 53.3,
+            "cleansheets": 9
+        },
+        {
+            "name": "HNK Gorica",
+            "location": "Velika Gorica",
+            "stadium": "Gradski stadion Velika Gorica",
+            "goals": 43,
+            "goalsconceded": 50,
+            "possessionpercentage": 48.6,
+            "cleansheets": 9
+        },
+        {
+            "name": "NK Lokomotiva",
+            "location": "Zagreb",
+            "stadium": "Kranjceviceva",
+            "goals": 55,
+            "goalsconceded": 50,
+            "possessionpercentage": 51.1,
+            "cleansheets": 6
+        },
+        {
+            "name": "NK Slaven Belupo",
+            "location": "Koprivnica",
+            "stadium": "Gradski stadion Ivan Kusek Apas",
+            "goals": 35,
+            "goalsconceded": 54,
+            "possessionpercentage": 46.3,
+            "cleansheets": 6
+        },
+        {
+            "name": "HNK Sibenik",
+            "location": "Sibenik",
+            "stadium": "Subicevac",
+            "goals": 46,
+            "goalsconceded": 75,
+            "possessionpercentage": 42.8,
+            "cleansheets": 1
+        },
+        {
+            "name": "NK Istra 1961",
+            "location": "Pula",
+            "stadium": "Aldo Drosina",
+            "goals": 42,
+            "goalsconceded": 67,
+            "possessionpercentage": 51.8,
+            "cleansheets": 5
+        },
+        {
+            "name": "NK Hrvatski Dragovoljac",
+            "location": "Zagreb",
+            "stadium": "Stadion NSC Stjepan Spajić",
+            "goals": 31,
+            "goalsconceded": 75,
+            "possessionpercentage": 38.5,
+            "cleansheets": 3
+        }
+    ]
+}
+
+var defaultBorderRadius = "10px";
