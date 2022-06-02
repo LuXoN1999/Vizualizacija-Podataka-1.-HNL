909283d3f7aba165467190c02e51b65d28db77b2
LuXoN1999 <llovretic99@gmail.com>
2022-06-02 22:51:00 +0200

    Added new design and added piecharts for Top4

--- mainVariables.js
+++ mainVariables.js
@@ -8,4 +8,31 @@ var defaultFontFamily = "Rockwell";
 var headerTextFontSize = "60px";
 var sectionTextFontSize = "40px";
 var normalTextFontSize = "30px";
+var smallTextFontSize = "20px";
+
+//==========CLUB PAGES ATTRIBUTES==========//
+
+//CLUB EMBLEMS
+var clubEmblemWidth = 150;
+var clubEmblemHeight = 150;
+var clubEmblemX = 50;
+var clubEmblemY = svgHeight / 15;
+
+//CLUB HEADER TEXT
+var clubHeaderTextX = 250;
+var clubHeaderTextY = svgHeight / 5;
+
+//CLUBS PAGE - TEXT COORDINATES
+var commonTextY = svgHeight / 3;
+var WDLTextX = 25;
+var playersTextX = 600;
+
+//CLUBS PAGE - COMMON PIE CHARTS COORDINATES
+var commonPieChartX = 660;
+var upperPieChartY = 375;
+var lowerPieChartY = upperPieChartY + 225;
+
+//PIE CHART ATTRIBUTES
+var outerRadius = 100;
+var innerRadius = 0;
 
